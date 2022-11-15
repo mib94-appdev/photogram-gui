73aa0238529f413d6c3da47fe60431f9a9140fe3
mib94 <mibnobachir@chicagobooth.edu>
2022-11-15 06:00:06 +0000

    user details looks good

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,20 @@ class UsersController < ApplicationController
 
     render ({ :template => "user_templates/index.html.erb" })
   end
+
+  def show
+    # Parameters: 
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({ :username => url_username })
+
+    @the_user = matching_usernames.at(0)
+
+    #if the_user == nil
+    #  redirect_to("/404")
+    #else
+
+    render({ :template => "user_templates/show.html.erb"})
+    #end
+  end
 end
