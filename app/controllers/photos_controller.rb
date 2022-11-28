5e722e56822823de0209243eba175d42b1df9e6d
mib94 <mibnobachir@chicagobooth.edu>
2022-11-28 04:46:54 +0000

    photos index completed

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,8 @@
 class PhotosController < ApplicationController
   def index
+    matching_photos = Photo.all
+
+    @list_of_photos = matching_photos.order({ :created_at => :desc })
 
     render({ :template => "photos_templates/index.html.erb"})
   end
